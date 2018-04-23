5fe9b95d2d6c2198927a00847e654bbfc5b856c2
Eric Fiorani <efiorani305@gmail.com>
2018-04-23 17:06:19 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
 def my_each(words) # put argument(s) here
-  # code here
+  if block_given?
+    i = 0
+
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+
+  array
+  else
+    puts "Hey! No block was given!"
+  end
 end
